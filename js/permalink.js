d2d977f74b5bfda8c9f9116b1251e8a2822a2f14
joseamirandavelez <joseamirandavelez@gmail.com>
2020-04-15 13:50:39 -0400

    not needed

--- js/permalink.js
+++
@@ -1,9 +0,0 @@
-function getPermalink() {
-	if (window.jQuery) {
-		const container = $('#permalink');
-		if (permalink != null) {
-			container.append(permalink);
-		}
-	}
-}
-$(document).ready(getPermalink);
