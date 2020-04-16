0ef2e3998985e720597eafd8a251ffe4d4904f0e
Pi-Hsun Shih <pihsun@chromium.org>
2020-04-16 21:44:25 +0800

    Change tab -> 2 space
    To align with all other .js files.

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,38 @@
 // PWA Code adapted from https://github.com/pwa-builder/PWABuilder
 const CACHE = "pwa-precache-v1";
 const precacheFiles = [
-	"/index.html",
-	"/js/predictions.js",
-	"/js/scripts.js",
-	"/css/styles.css",
-	"https://code.jquery.com/jquery-3.4.1.min.js",
+  "/index.html",
+  "/js/predictions.js",
+  "/js/scripts.js",
+  "/css/styles.css",
+  "https://code.jquery.com/jquery-3.4.1.min.js",
 ];
 
 self.addEventListener("install", function (event) {
-	console.log("[PWA] Install Event processing");
+  console.log("[PWA] Install Event processing");
 
-	console.log("[PWA] Skip waiting on install");
-	self.skipWaiting();
+  console.log("[PWA] Skip waiting on install");
+  self.skipWaiting();
 
-	event.waitUntil(
-		caches.open(CACHE).then(function (cache) {
-			console.log("[PWA] Caching pages during install");
-			return cache.addAll(precacheFiles);
-		})
-	);
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA] Caching pages during install");
+      return cache.addAll(precacheFiles);
+    })
+  );
 });
 
 // Allow sw to control of current page
 self.addEventListener("activate", function (event) {
-	console.log("[PWA] Claiming clients for current page");
-	event.waitUntil(self.clients.claim());
+  console.log("[PWA] Claiming clients for current page");
+  event.waitUntil(self.clients.claim());
 });
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function (event) {
-	if (event.request.method !== "GET") return;
+  if (event.request.method !== "GET") return;
 
-	event.respondWith(
+  event.respondWith(
     (async () => {
       let response;
       try {
@@ -50,26 +50,26 @@ self.addEventListener("fetch", function (event) {
       }
       return response;
     })()
-	);
+  );
 });
 
 function fromCache(request) {
-	// Check to see if you have it in the cache
-	// Return response
-	// If not in the cache, then return
-	return caches.open(CACHE).then(function (cache) {
-		return cache.match(request).then(function (matching) {
-			if (!matching || matching.status === 404) {
-				return Promise.reject("no-match");
-			}
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
 
-			return matching;
-		});
-	});
+      return matching;
+    });
+  });
 }
 
 function updateCache(request, response) {
-	return caches.open(CACHE).then(function (cache) {
-		return cache.put(request, response);
-	});
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
 }
