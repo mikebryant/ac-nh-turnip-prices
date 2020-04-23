58afb74d460599b59d7a54aaa40b4cd9f0cfedcb
Rex Tsou <rex.tsou@kkday.com>
2020-04-23 13:26:54 +0800

    Update contributors.js for better performance
    append element to the DOM too many times may cause performance issue,
    so do it only once.

--- js/contributors.js
+++ js/contributors.js
@@ -3,12 +3,14 @@ function getContributors() {
     const container = $('#contributors');
     jQuery.ajax('https://api.github.com/repos/mikebryant/ac-nh-turnip-prices/contributors', {})
       .done(function (data) {
+        const contributorList = [];
         data.forEach((contributor, idx) => {
-          container.append(`<a href="${contributor.html_url}">${contributor.login}</a>`);
+          contributorList.push(`<a href="${contributor.html_url}">${contributor.login}</a>`);
           if (idx < data.length - 1) {
-            container.append(', ');
+            contributorList.push(', ');
           }
         });
+        container.append(contributorList.join(''));
       });
   }
 }
