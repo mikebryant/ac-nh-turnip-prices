db1aaea08133f0165f4103bc037638a9d48b9f68
James Glenn <jglenn@riotgames.com>
2020-04-30 13:48:30 -0500

    pull out 100 per page into its own const

--- js/contributors.js
+++ js/contributors.js
@@ -1,7 +1,8 @@
 function getContributors(page) {
+  const PER_PAGE = 100
   if (window.jQuery) {
     const container = $('#contributors');
-    jQuery.ajax(`https://api.github.com/repos/mikebryant/ac-nh-turnip-prices/contributors?page=${page}&per_page=100`, {})
+    jQuery.ajax(`https://api.github.com/repos/mikebryant/ac-nh-turnip-prices/contributors?page=${page}&per_page=${PER_PAGE}`, {})
       .done(function (data) {
         const contributorList = [];
         data.forEach((contributor, idx) => {
@@ -15,8 +16,8 @@ function getContributors(page) {
           }
         });
         container.append(contributorList.join(''));
-        // If the length of the data is < 100, we know we are processing the last page of data.
-        if (data.length < 100) return; 
+        // If the length of the data is < PER_PAGE, we know we are processing the last page of data.
+        if (data.length < PER_PAGE) return; 
         getContributors(page + 1);
       });
   }
