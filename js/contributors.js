a797064fb83fde0450973062374e67f66dc75bb8
James Glenn <jglenn@riotgames.com>
2020-04-30 11:20:36 -0500

    add pagination support for fetching contributors from Github

--- js/contributors.js
+++ js/contributors.js
@@ -1,18 +1,25 @@
-function getContributors() {
+function getContributors(page) {
   if (window.jQuery) {
-    const container = $('#contributors');
-    jQuery.ajax('https://api.github.com/repos/mikebryant/ac-nh-turnip-prices/contributors', {})
+    const container = $("#contributors");
+    jQuery.ajax(`https://api.github.com/repos/mikebryant/ac-nh-turnip-prices/contributors?page=${page}`, {})
       .done(function (data) {
+        if (data.length === 0) {
+          return;
+        }
+
         const contributorList = [];
         data.forEach((contributor, idx) => {
-          contributorList.push(`<a href="${contributor.html_url}">${contributor.login}</a>`);
+          contributorList.push(
+            `<a href="${contributor.html_url}">${contributor.login}</a>`
+          );
           if (idx < data.length - 1) {
-            contributorList.push(', ');
+            contributorList.push(", ");
           }
         });
-        container.append(contributorList.join(''));
+        container.append(contributorList.join(""));
+        getContributors(page + 1);
       });
   }
 }
 
-$(document).ready(getContributors);
+$(document).ready(getContributors(1));
